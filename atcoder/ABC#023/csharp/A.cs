084e1de954348b84a960c73d4fe09dda48fdc803
setchi <ri_xer@yahoo.co.jp>
2015-06-11 17:31:31 +0900

    Solve ABC#023-A

---
+++ atcoder/ABC#023/csharp/A.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+	    string str = Console.ReadLine();
+
+	    int a = int.Parse(str.Substring(0, 1));
+	    int b = int.Parse(str.Substring(1, 1));
+
+        Console.WriteLine(a + b);
+	}
+}
