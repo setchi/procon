b1c4c1a1fd414324e23b3a9ec1383f983fab7230
setchi <ri_xer@yahoo.co.jp>
2015-06-11 17:30:54 +0900

    Solve ABC#024-A

---
+++ atcoder/ABC#024/csharp/A.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+	    int[] line = ReadLine();
+	    int[] line2 = ReadLine();
+
+	    int a = line[0];
+	    int b = line[1];
+	    int c = line[2];
+	    int k = line[3];
+
+	    int s = line2[0];
+	    int t = line2[1];
+
+        Console.WriteLine(a*s+b*t-(s+t<k?0:s+t)*c);
+	}
+
+	static int[] ReadLine() {
+	    return Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+	}
+}
