5df53ea739f4d03996dcd817c0d9c7b5a43bf0c5
setchi <ri_xer@yahoo.co.jp>
2015-06-11 23:00:11 +0900

    Solve ABC#023-B

---
+++ atcoder/ABC#023/csharp/B.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int N = int.Parse(Console.ReadLine());
+		string S = Console.ReadLine();
+
+		int res = -1;
+		string str = "b";
+		string[] chars = {"a", "c", "b"};
+
+		for (int i = 0; i < N; i++) {
+		    if (S == str) {
+		        res = i;
+		        break;
+		    }
+			str = chars[i % 3] + str + chars[2 - (i + 1) % 3];
+		}
+
+        Console.WriteLine(res);
+	}
+}
