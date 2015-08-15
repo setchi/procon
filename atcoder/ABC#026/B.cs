b8f39429f726d1b6bad7c49d0399f48484820d79
setchi <ri_xer@yahoo.co.jp>
2015-08-15 20:24:47 +0900

    Solve ABC#026-B

---
+++ atcoder/ABC#026/B.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+class Program {
+    public static void Main() {
+        var N = int.Parse(Console.ReadLine());
+
+        var S = Enumerable.Range(0, N)
+          .Select(_ => int.Parse(Console.ReadLine()))
+          .Select(r => r * r)
+          .OrderByDescending(r => r)
+          .Select((r, i) => i % 2 == 0 ? r : -r)
+          .Aggregate((a, c) => a + c)
+          * Math.PI;
+
+        Console.WriteLine(S);
+    }
+}
