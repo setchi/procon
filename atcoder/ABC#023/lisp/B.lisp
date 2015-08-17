e856650e23e75e43172152de76c3945ac56eed06
setchi <ri_xer@yahoo.co.jp>
2015-08-17 16:00:39 +0900

    Solve ABC#023-B / lisp

---
+++ atcoder/ABC#023/lisp/B.lisp
@@ -0,0 +1,8 @@
+(defun solve (n s i r)
+	(if (<= n (length r))
+		(if (equalp s r) i -1)
+		(solve n s (+ i 1) (format nil "~a~a~a" (nth (mod i 3) `(a c b)) r (nth (mod i 3) `(c a b))))))
+ 
+(defun main ()
+	(format t "~a~%" (solve (read) (read-line) 0 "b")))
+(main)
