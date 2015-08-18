47c2a582702a39cad5b30146c43e5ebd9097bd42
setchi <ri_xer@yahoo.co.jp>
2015-08-18 14:49:27 +0900

    Solve ABC#025-A / lisp

---
+++ atcoder/ABC#025/lisp/A.lisp
@@ -0,0 +1,7 @@
+(defun solve (s n)
+	(let ((s (coerce s `list)) (n (- n 1)))
+		`(,(nth (floor (/ n 5)) s) ,(nth (mod n 5) s))))
+
+(defun main ()
+	(format t "~{~a~}~%" (solve (read-line) (read))))
+(main)
