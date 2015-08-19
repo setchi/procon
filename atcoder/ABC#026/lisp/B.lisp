e40084ea1668b3ac50ee397b33e249d74d913c37
setchi <ri_xer@yahoo.co.jp>
2015-08-19 12:35:19 +0900

    more ABC#026-B / lisp

--- atcoder/ABC#026/lisp/B.lisp
+++ atcoder/ABC#026/lisp/B.lisp
@@ -1,7 +1,7 @@
 (defun solve (n)
   (* pi (loop for i from 0
-              for r2 in (sort (loop repeat n collect (expt (read) 2)) #'>)
-              sum (if (evenp i) r2 (- r2)))))
+              for r in (sort (loop repeat n collect (read)) #'>)
+              sum (* (expt r 2) (expt -1 i)))))
 
 (defun main ()
   (format t "~,7f~%" (solve (read))))
