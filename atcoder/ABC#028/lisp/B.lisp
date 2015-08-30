dfdef582f9d4c1fc740d2a5a7e14c8188629398a
setchi <ri_xer@yahoo.co.jp>
2015-08-30 12:11:34 +0900

    Fix format

--- atcoder/ABC#028/lisp/B.lisp
+++ atcoder/ABC#028/lisp/B.lisp
@@ -3,4 +3,4 @@
           (loop for i from (char-code #\A) to (char-code #\F)
                 collect (count (code-char i) s))))
 
-(format t "~a~&" (solve (loop repeat 5 collect (read))))
+(format t "~a~%" (solve (loop repeat 5 collect (read))))
