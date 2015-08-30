8f3d39be2a934834e845a55994ffefbfcefa1c60
setchi <ri_xer@yahoo.co.jp>
2015-08-30 12:09:41 +0900

    Solve ABC#028-B / lisp

---
+++ atcoder/ABC#028/lisp/B.lisp
@@ -0,0 +1,6 @@
+(defun solve (s)
+  (format nil "~{~A~^ ~}"
+          (loop for i from (char-code #\A) to (char-code #\F)
+                collect (count (code-char i) s))))
+
+(format t "~a~&" (solve (loop repeat 5 collect (read))))
