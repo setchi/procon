a9ee93907975d25878a55fa19a0acd0fe251639e
setchi <ri_xer@yahoo.co.jp>
2015-08-31 18:05:26 +0900

    more ABC#026-C / lisp

--- atcoder/ABC#026/lisp/C.lisp
+++ atcoder/ABC#026/lisp/C.lisp
@@ -1,15 +1,13 @@
 (defun calc-salary (b i)
-  (let ((b-i-sub (aref b i))) 
-    (if (zerop (length b-i-sub)) 1
-        (loop for j from 0 to (1- (length b-i-sub))
-              maximizing (calc-salary b (nth j b-i-sub)) into max
-              minimizing (calc-salary b (nth j b-i-sub)) into min
-              finally (return (+ max min 1))))))
+  (if (zerop (length (nth i b))) 1
+      (loop for j in (nth i b)
+            maximizing (calc-salary b j) into max
+            minimizing (calc-salary b j) into min
+            finally (return (+ max min 1)))))
 
 (defun main ()
-  (let* ((n (read)) (b (make-array n :initial-contents (loop repeat n collect nil))))
-    (loop for i from 1 to (1- n)
-          for i-boss = (1- (read))
-          do (push i (aref b i-boss)))
+  (let* ((n (read)) (b (loop repeat n collect nil)))
+    (dotimes (i (1- n))
+      (push (1+ i) (nth (1- (read)) b)))
     (format t "~a~%" (calc-salary b 0))))
 (main)
